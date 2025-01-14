49ab2ff192d6066794f9e0dc8fdd0451648a5b56
Zahran Yahia Khan <114562123+zahran001@users.noreply.github.com>
2025-01-13 21:36:05 -0500

    Created ApplicationDbContext for Coupon

---
+++ E-commerce.Services.CouponAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using E_commerce.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_commerce.Services.CouponAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Constructor
+        // The constructor accepts DbContextOptions of type ApplicationDbContext.
+        // These options are passed to the base DbContext class to configure the context.
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // DbSet
+        // This is the table that we are going to create in the database.
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
---
+++ E-commerce.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commerce.Services.CouponAPI.Models
+{
+    public class Coupon
+    {
+        [Key]
+        public int CouponId { get; set; }
+        [Required]
+        public string CouponCode { get; set; }
+        [Required]
+        public double DiscountAmount { get; set; }
+        [Required]
+        public int MinimumAmount { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
