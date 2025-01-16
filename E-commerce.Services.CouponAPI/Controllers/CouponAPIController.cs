0bbc7947d5b2ec5044662ad9f27acec0171e1314
Zahran Yahia Khan <114562123+zahran001@users.noreply.github.com>
2025-01-15 20:56:22 -0500

    Added a common ResponseDto

--- E-commerce.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ E-commerce.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -1,4 +1,5 @@
 ﻿using E_commerce.Services.CouponAPI.Data;
+using E_commerce.Services.CouponAPI.Dto;
 using E_commerce.Services.CouponAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,42 +13,54 @@ namespace E_commerce.Services.CouponAPI.Controllers
         // retrieve all coupons
         // In order to retrieve the records, we will be using EF Core, so we need ApplicationDbContext using DI.
         private readonly ApplicationDbContext _db;
+        //return ResponseDto in the controller
+        private ResponseDto _response;
+
+        
 
         // Constructor
         public CouponAPIController(ApplicationDbContext db)
         {
             _db = db;
+            _response = new ResponseDto(); // initialize
         }
 
         [HttpGet]
-        public object Get()
+        public ResponseDto Get()
         {
             try
             {
                 IEnumerable<Coupon> objList = _db.Coupons.ToList();
-                return objList;
+                _response.Result = objList;
             }
             catch (Exception ex)
             {
-                return StatusCode(500, new { message = "An error occurred while retrieving coupons.", error = ex.Message });
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
             }
+            // return back the response
+            return _response;
         }
 
         // get coupon by id
         [HttpGet]
         [Route("{id:int}")]
-        public object Get(int id)
+        public ResponseDto Get(int id)
         {
             try
             {
                 Coupon obj = _db.Coupons.First(u=>u.CouponId == id);
-                return obj;
+                _response.Result = obj;
             }
             catch (Exception ex)
             {
-                return StatusCode(500, new { message = "An error occurred while retrieving coupons.", error = ex.Message });
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
             }
+            return _response;
         }
 
     }
 }
+// Architecture for the API response: Whenever multiple APIs are being consumed, the response will be in one object format.
+// We want to have a common response for all the endpoints.
