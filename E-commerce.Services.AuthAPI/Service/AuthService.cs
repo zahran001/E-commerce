8ac6164408402b552a12a9760ea372fa40c984df
Zahran Yahia Khan <114562123+zahran001@users.noreply.github.com>
2025-02-07 01:57:56 -0500

    Implemented IAuthService

---
+++ E-commerce.Services.AuthAPI/Service/AuthService.cs
@@ -0,0 +1,18 @@
+﻿using E_commerce.Services.AuthAPI.Models.Dto;
+using E_commerce.Services.AuthAPI.Service.IService;
+
+namespace E_commerce.Services.AuthAPI.Service
+{
+	public class AuthService : IAuthService
+	{
+		public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ E-commerce.Services.AuthAPI/Service/IService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using E_commerce.Services.AuthAPI.Models.Dto;
+
+namespace E_commerce.Services.AuthAPI.Service.IService
+{
+	public interface IAuthService
+	{
+		Task<UserDto> Register(RegistrationRequestDto registrationRequestDto); // When a user is registering, the return type would be UserDto.
+		Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto); // When a user is logging in, the return type would be LoginResponseDto.
+	}
+}
