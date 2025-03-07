c503ca7de3546198e0a94211ec7ecd9f639379cd
Zahran Yahia Khan <114562123+zahran001@users.noreply.github.com>
2025-03-07 02:24:51 -0500

    Implemented login and register UI

---
+++ E-commerce.Web/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using E_commerce.Web.Models;
+using E_commerce.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_commerce.Web.Controllers
+{
+	public class AuthController : Controller
+	{
+		private readonly IAuthService _authService;
+		public AuthController(IAuthService authService)
+		{
+			_authService = authService;
+		}
+
+		[HttpGet]
+		public IActionResult Login()
+		{
+			LoginRequestDto loginRequestDto = new();
+			return View(loginRequestDto);
+		}
+
+		[HttpGet]
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+        public IActionResult Logout()
+        {
+			return View();
+        }
+
+    }
+}
